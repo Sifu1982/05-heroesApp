d1aec822e3bc64fc2a659e2ec38486c34b26f2e3
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-01 20:21:05 +0200

    feat: listado-component working

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Heroe } from '../interfaces/heroes.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroesService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getHeroes(): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+  }
 }
